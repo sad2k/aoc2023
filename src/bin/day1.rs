246969aeeb51ef4d9d035696e5c491f384bf7fd5
Sergey Dubakov <serge.dubakov@gmail.com>
2023-12-01 21:41:18 +0000

    day 1 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
 fn main() {
-    
+    let contents = fs::read_to_string("inputs/day1.txt").unwrap();
+    let lines = contents.lines();
+
+    let mut res = 0;
+    for line in lines {
+        let digits: Vec<u32> = line
+            .chars()
+            .map(|c| c.to_digit(10))
+            .filter(|o| o.is_some())
+            .map(|o| o.unwrap())
+            .collect();
+        res += digits[0] * 10 + digits[digits.len() - 1];
+    }
+    println!("{}", res);
 }
