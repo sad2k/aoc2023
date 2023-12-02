ca59d08cc98b22bfa76925295bfb88d1235ccb0e
Sergey Dubakov <serge.dubakov@gmail.com>
2023-12-02 19:07:19 +0000

    day 2 part 1

---
+++ src/bin/day2.rs
@@ -0,0 +1,54 @@
+use std::{fs, str::Lines};
+
+#[derive(Debug)]
+struct CubeSet {
+    red: u32,
+    green: u32,
+    blue: u32
+}
+
+fn parse(line: &str) -> Vec<CubeSet> {
+    let mut res = Vec::new();
+    let sets = line.split(": ").collect::<Vec<_>>()[1];
+    for s in sets.split("; ") {
+        let cubes = s.split(", ");
+        let mut cs = CubeSet {
+            red: 0,
+            green: 0,
+            blue: 0
+        };
+        for cube in cubes {
+            let split_cube = cube.split(" ").collect::<Vec<_>>();
+            let num: u32 = split_cube[0].parse().unwrap();
+            match split_cube[1] {
+                "red" => cs.red = num,
+                "green" => cs.green = num,
+                "blue" => cs.blue = num,
+                _ => panic!("unexpected colour: {}", split_cube[1])
+            }
+        }
+        res.push(cs);
+    } 
+    res
+}
+
+fn part1(lines: Lines<'_>) -> u32 {
+    let mut res = 0;
+    let mut game = 0;
+    for line in lines {
+        game += 1;
+        let parsed_sets = parse(line);
+        let bad_sets: Vec<_> = parsed_sets.iter().filter(|cs| cs.red > 12 || cs.green > 13 || cs.blue > 14).collect();
+        if bad_sets.is_empty() {
+            res += game;
+        }
+    }
+    res
+}
+
+fn main() {
+    let contents = fs::read_to_string("inputs/day2.txt").unwrap();
+
+    // part 1
+    println!("{}", part1(contents.lines()));
+}
