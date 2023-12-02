5a2a55b5d206f547238eb08fcc5d122126e889d2
Sergey Dubakov <serge.dubakov@gmail.com>
2023-12-02 19:24:08 +0000

    day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,7 +4,21 @@ use std::{fs, str::Lines};
 struct CubeSet {
     red: u32,
     green: u32,
-    blue: u32
+    blue: u32,
+}
+
+impl CubeSet {
+    fn power(&self) -> u64 {
+        self.red as u64 * self.green as u64 * self.blue as u64
+    }
+
+    fn max(&self, other: &CubeSet) -> CubeSet {
+        CubeSet { red: self.red.max(other.red), green: self.green.max(other.green), blue: self.blue.max(other.blue) }
+    }
+
+    fn empty() -> CubeSet {
+        CubeSet { red: 0, green: 0, blue: 0 }
+    }
 }
 
 fn parse(line: &str) -> Vec<CubeSet> {
@@ -15,7 +29,7 @@ fn parse(line: &str) -> Vec<CubeSet> {
         let mut cs = CubeSet {
             red: 0,
             green: 0,
-            blue: 0
+            blue: 0,
         };
         for cube in cubes {
             let split_cube = cube.split(" ").collect::<Vec<_>>();
@@ -24,11 +38,11 @@ fn parse(line: &str) -> Vec<CubeSet> {
                 "red" => cs.red = num,
                 "green" => cs.green = num,
                 "blue" => cs.blue = num,
-                _ => panic!("unexpected colour: {}", split_cube[1])
+                _ => panic!("unexpected colour: {}", split_cube[1]),
             }
         }
         res.push(cs);
-    } 
+    }
     res
 }
 
@@ -38,10 +52,25 @@ fn part1(lines: Lines<'_>) -> u32 {
     for line in lines {
         game += 1;
         let parsed_sets = parse(line);
-        let bad_sets: Vec<_> = parsed_sets.iter().filter(|cs| cs.red > 12 || cs.green > 13 || cs.blue > 14).collect();
+        let bad_sets: Vec<_> = parsed_sets
+            .iter()
+            .filter(|cs| cs.red > 12 || cs.green > 13 || cs.blue > 14)
+            .collect();
         if bad_sets.is_empty() {
             res += game;
         }
+    }
+    res
+}
+
+fn part2(lines: Lines<'_>) -> u64 {
+    let mut res = 0;
+    let mut game = 0;
+    for line in lines {
+        game += 1;
+        let parsed_sets = parse(line);
+        let power_set = parsed_sets.iter().fold(CubeSet::empty(), |acc,x| acc.max(x));
+        res += power_set.power();
     }
     res
 }
@@ -50,5 +79,8 @@ fn main() {
     let contents = fs::read_to_string("inputs/day2.txt").unwrap();
 
     // part 1
-    println!("{}", part1(contents.lines()));
+    // println!("{}", part1(contents.lines()));
+
+    // part 2
+    println!("{}", part2(contents.lines()));
 }
