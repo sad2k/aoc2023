901d727bb258b89417db9af1bf3a676a27a47ba2
Sergey Dubakov <serge.dubakov@gmail.com>
2023-12-03 17:49:51 +0000

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,39 +1,90 @@
-use std::{fs, str::Lines};
+use std::{collections::HashMap, fs, str::Lines};
 
 fn is_symbol(ch: char) -> bool {
     ch != '.' && !ch.is_digit(10)
+}
+
+fn is_gear(ch: char) -> bool {
+    ch == '*'
 }
 
 fn is_adjacent_to_symbol(lines: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
     let mut res = false;
     if (row > 0) {
         // previous row
-        res |= is_symbol(lines[row-1][col]);
+        res |= is_symbol(lines[row - 1][col]);
         if col > 0 {
-            res |= is_symbol(lines[row-1][col-1]);
+            res |= is_symbol(lines[row - 1][col - 1]);
         }
-        if col < lines[0].len()-1 {
-            res |= is_symbol(lines[row-1][col+1]);
+        if col < lines[0].len() - 1 {
+            res |= is_symbol(lines[row - 1][col + 1]);
         }
     }
-    if (row < lines.len()-1) {
+    if (row < lines.len() - 1) {
         // next row
-        res |= is_symbol(lines[row+1][col]);
+        res |= is_symbol(lines[row + 1][col]);
         if col > 0 {
-            res |= is_symbol(lines[row+1][col-1]);
+            res |= is_symbol(lines[row + 1][col - 1]);
         }
-        if col < lines[0].len()-1 {
-            res |= is_symbol(lines[row+1][col+1]);
+        if col < lines[0].len() - 1 {
+            res |= is_symbol(lines[row + 1][col + 1]);
         }
     }
     // current row
     if col > 0 {
-        res |= is_symbol(lines[row][col-1]);
+        res |= is_symbol(lines[row][col - 1]);
     }
-    if col < lines[0].len()-1 {
-        res |= is_symbol(lines[row][col+1]);
+    if col < lines[0].len() - 1 {
+        res |= is_symbol(lines[row][col + 1]);
     }
     res
+}
+
+fn is_adjacent_to_gear(lines: &Vec<Vec<char>>, row: usize, col: usize) -> Option<(usize, usize)> {
+    if (row > 0) {
+        // previous row
+        if is_gear(lines[row - 1][col]) {
+            return Some((row - 1, col));
+        }
+        if col > 0 {
+            if is_gear(lines[row - 1][col - 1]) {
+                return Some((row - 1, col - 1));
+            }
+        }
+        if col < lines[0].len() - 1 {
+            if is_gear(lines[row - 1][col + 1]) {
+                return Some((row - 1, col + 1));
+            }
+        }
+    }
+    if (row < lines.len() - 1) {
+        // next row
+        if is_gear(lines[row + 1][col]) {
+            return Some((row + 1, col));
+        }
+        if col > 0 {
+            if is_gear(lines[row + 1][col - 1]) {
+                return Some((row + 1, col - 1));
+            }
+        }
+        if col < lines[0].len() - 1 {
+            if is_gear(lines[row + 1][col + 1]) {
+                return Some((row + 1, col + 1));
+            }
+        }
+    }
+    // current row
+    if col > 0 {
+        if is_gear(lines[row][col - 1]) {
+            return Some((row, col - 1));
+        }
+    }
+    if col < lines[0].len() - 1 {
+        if is_gear(lines[row][col + 1]) {
+            return Some((row, col + 1));
+        }
+    }
+    None
 }
 
 fn part1(lines: Lines<'_>) -> u64 {
@@ -42,7 +93,6 @@ fn part1(lines: Lines<'_>) -> u64 {
     let mut is_part_number = false;
     let mut part_numbers = Vec::new();
     for i in 0..l.len() {
-        // println!("{:?}", l[i]);
         let line = &l[i];
         s.clear();
         is_part_number = false;
@@ -70,9 +120,61 @@ fn part1(lines: Lines<'_>) -> u64 {
     part_numbers.iter().map(|x| x.parse::<u64>().unwrap()).sum()
 }
 
+fn part2(lines: Lines<'_>) -> u64 {
+    let l: Vec<_> = lines.map(|x| x.chars().collect::<Vec<_>>()).collect();
+    let mut s = String::new();
+    let mut gear_of: Option<(usize, usize)> = None;
+    let mut gears = HashMap::new();
+    for i in 0..l.len() {
+        let line = &l[i];
+        s.clear();
+        gear_of = None;
+        for j in 0..line.len() {
+            let ch = line[j];
+            if ch.is_digit(10) {
+                s.push(ch);
+                let g = is_adjacent_to_gear(&l, i, j);
+                if g.is_some() {
+                    gear_of = g;
+                }
+            } else {
+                if !s.is_empty() {
+                    if gear_of.is_some() {
+                        //gears.insert(gear_of.clone().unwrap(), s.parse::<u64>());
+                        gears
+                            .entry(gear_of.clone().unwrap())
+                            .or_insert(Vec::new())
+                            .push(s.parse::<u64>().unwrap());
+                    }
+                    s.clear();
+                    gear_of = None;
+                }
+            }
+        }
+        if !s.is_empty() {
+            if gear_of.is_some() {
+                gears
+                    .entry(gear_of.clone().unwrap())
+                    .or_insert(Vec::new())
+                    .push(s.parse::<u64>().unwrap());
+            }
+        }
+    }
+    gears
+        .iter()
+        .map(|x| if x.1.len() == 2 { Some(x.1) } else { None })
+        .filter(|x| x.is_some())
+        .map(|x| x.unwrap())
+        .map(|x| x[0] * x[1])
+        .sum()
+}
+
 fn main() {
     let contents = fs::read_to_string("inputs/day3.txt").unwrap();
 
     // part 1
-    println!("{}", part1(contents.lines()));
+    // println!("{}", part1(contents.lines()));
+
+    // part 2
+    println!("{}", part2(contents.lines()));
 }
