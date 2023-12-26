9a0b5cc9541c6d731736721140b02f3c64a3ac8e
Sergey Dubakov <serge.dubakov@gmail.com>
2023-12-26 16:22:05 +0000

    still debugging

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,18 +1,20 @@
 use std::fs;
 
-fn solve0(v: &Vec<u64>) -> Option<usize> {
+fn solve0(v: &Vec<u64>, old_best: Option<usize>) -> Option<usize> {
     let mut best: Option<usize> = None;
     for i in 0..(v.len() - 1) {
         let max_len = (i + 1).min(v.len() - i - 1);
         let slice1 = &v[0..i + 1].iter().rev().map(|x| *x).collect::<Vec<_>>()[0..max_len];
         let slice2 = &v[i + 1..(v.len()).min(i + 1 + max_len)];
         if (slice1 == slice2) {
-            if let Some(b) = best {
-                if i + 1 > b {
+            if old_best.is_none() || old_best.unwrap() != i + 1 {
+                if let Some(b) = best {
+                    if i + 1 > b {
+                        best = Some(i + 1);
+                    }
+                } else {
                     best = Some(i + 1);
                 }
-            } else {
-                best = Some(i + 1);
             }
         }
     }
@@ -29,7 +31,7 @@ fn solve(pattern: &Vec<String>) -> u64 {
         .iter()
         .map(|x| u64::from_str_radix(&x, 2).unwrap())
         .collect::<Vec<_>>();
-    let best_horiz = solve0(&horiz);
+    let best_horiz = solve0(&horiz, None);
     if best_horiz.is_some() {
         return (best_horiz.unwrap() * 100) as u64;
     }
@@ -51,7 +53,7 @@ fn solve(pattern: &Vec<String>) -> u64 {
         .iter()
         .map(|x| u64::from_str_radix(&x, 2).unwrap())
         .collect::<Vec<_>>();
-    return solve0(&vert_nums).unwrap_or(0) as u64;
+    return solve0(&vert_nums, None).unwrap_or(0) as u64;
 }
 
 fn part1(lines: &Vec<Vec<String>>) -> u64 {
@@ -62,14 +64,14 @@ fn part1(lines: &Vec<Vec<String>>) -> u64 {
     res
 }
 
-fn solve0_with_smudges(v: &Vec<u64>, bits: usize) -> Option<usize> {
+fn solve0_with_smudges(v: &Vec<u64>, bits: usize, old_best: Option<usize>) -> Option<usize> {
     let mut vv = v.clone();
     for i in 0..vv.len() {
         let orig = vv[i];
         for j in 0..bits {
             let modif = orig ^ (1 << j);
             vv[i] = modif;
-            let res = solve0(&vv);
+            let res = solve0(&vv, old_best);
             if res.is_some() {
                 // println!("{} {}", i, j);
                 return res;
@@ -90,7 +92,8 @@ fn solve2(pattern: &Vec<String>) -> u64 {
         .iter()
         .map(|x| u64::from_str_radix(&x, 2).unwrap())
         .collect::<Vec<_>>();
-    let best_horiz = solve0_with_smudges(&horiz, binary[0].len());
+    let old_best_horiz = solve0(&horiz, None);
+    let best_horiz = solve0_with_smudges(&horiz, binary[0].len(), old_best_horiz);
     if best_horiz.is_some() {
         return (best_horiz.unwrap() * 100) as u64;
     }
@@ -113,7 +116,8 @@ fn solve2(pattern: &Vec<String>) -> u64 {
         .map(|x| u64::from_str_radix(&x, 2).unwrap())
         .collect::<Vec<_>>();
 
-    return solve0_with_smudges(&vert_nums, binary_chars[0].len()).unwrap_or(0) as u64;
+    let old_best_vert = solve0(&vert_nums, None);
+    return solve0_with_smudges(&vert_nums, binary_chars[0].len(), old_best_vert).unwrap_or(0) as u64;
 }
 
 fn part2(lines: &Vec<Vec<String>>) -> u64 {
