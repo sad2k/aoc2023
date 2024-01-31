ff311ec1009b9f50a36116ce18f7a471cc43a9bf
Sergey Dubakov <serge.dubakov@gmail.com>
2024-01-31 22:36:29 +0000

    prep for contraction

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -5,17 +5,17 @@ use std::{
 
 #[derive(Debug)]
 struct Graph {
-    node_edges: HashMap<(i32, i32), Vec<(i32, i32)>>,
+    nodes: HashMap<(i32, i32), Vec<(i32, i32, i32)>>,
 }
 
 #[derive(Debug)]
 struct Path {
-    previous: HashSet<(i32, i32)>,
-    last: (i32, i32),
+    previous: HashSet<(i32, i32, i32)>,
+    last: (i32, i32, i32),
 }
 
 fn parse_graph(lines: &Vec<Vec<char>>, use_slopes: bool) -> Graph {
-    let mut node_edges: HashMap<(i32, i32), Vec<(i32, i32)>> = HashMap::new();
+    let mut node_edges: HashMap<(i32, i32), Vec<(i32, i32, i32)>> = HashMap::new();
     for row in 0..lines.len() {
         let line = &lines[row];
         for col in 0..line.len() {
@@ -29,23 +29,23 @@ fn parse_graph(lines: &Vec<Vec<char>>, use_slopes: bool) -> Graph {
                 '.' => {
                     let mut v = Vec::new();
                     if row > 0 && lines[row - 1][col] != '#' {
-                        v.push(((row - 1) as i32, col as i32));
+                        v.push(((row - 1) as i32, col as i32, 1));
                     }
                     if row < (lines.len() - 1) && lines[row + 1][col] != '#' {
-                        v.push(((row + 1) as i32, col as i32));
+                        v.push(((row + 1) as i32, col as i32, 1));
                     }
                     if col > 0 && lines[row][col - 1] != '#' {
-                        v.push((row as i32, (col - 1) as i32));
+                        v.push((row as i32, (col - 1) as i32, 1));
                     }
                     if col < (line.len() - 1) && lines[row][col + 1] != '#' {
-                        v.push((row as i32, (col + 1) as i32));
+                        v.push((row as i32, (col + 1) as i32, 1));
                     }
                     Some(v)
                 }
-                '>' => Some(vec![(row as i32, (col + 1) as i32)]),
-                '<' => Some(vec![(row as i32, (col - 1) as i32)]),
-                'v' => Some(vec![((row + 1) as i32, col as i32)]),
-                '^' => Some(vec![((row - 1) as i32, col as i32)]),
+                '>' => Some(vec![(row as i32, (col + 1) as i32, 1)]),
+                '<' => Some(vec![(row as i32, (col - 1) as i32, 1)]),
+                'v' => Some(vec![((row + 1) as i32, col as i32, 1)]),
+                '^' => Some(vec![((row - 1) as i32, col as i32, 1)]),
                 _ => None,
             };
 
@@ -54,25 +54,26 @@ fn parse_graph(lines: &Vec<Vec<char>>, use_slopes: bool) -> Graph {
             }
         }
     }
-    Graph { node_edges }
+    Graph { nodes: node_edges }
 }
 
 fn solve(graph: &Graph, dest: (i32, i32)) -> i32 {
     let mut q = VecDeque::new();
     q.push_front(Path {
         previous: HashSet::new(),
-        last: (0, 1),
+        last: (0, 1, 0),
     });
     let mut cnt = 0;
     let mut res = 0;
     while !q.is_empty() {
         let p = q.pop_front().unwrap();
-        if p.last == dest {
-            res = res.max(p.previous.len());
+        if p.last.0 == dest.0 && p.last.1 == dest.1 {
+            let path_len = p.previous.iter().map(|x| x.2).sum::<i32>() + p.last.2;
+            res = res.max(path_len);
             cnt += 1;
-            println!("{}: max so far {}", cnt, res)
+            // println!("{}: max so far {}", cnt, res)
         } else {
-            for e in &graph.node_edges[&p.last] {
+            for e in &graph.nodes[&(p.last.0, p.last.1)] {
                 if !p.previous.contains(e) {
                     let mut new_prev = p.previous.clone();
                     new_prev.insert(p.last);
@@ -85,6 +86,16 @@ fn solve(graph: &Graph, dest: (i32, i32)) -> i32 {
         }
     }
     res as i32
+}
+
+fn contract(graph: &Graph) -> Graph {
+    // let mut new_node_edges = HashMap::new();
+    // for (n, edges) in &graph.node_edges {
+
+    // } 
+    Graph {
+        nodes: graph.nodes.clone()
+    }
 }
 
 fn main() {
@@ -100,6 +111,8 @@ fn main() {
 
     // part 2
     let graph2 = parse_graph(&lines, false);
+    let graph2 = contract(&graph2);
+    println!("{:?}", graph); 
     println!(
         "{}",
         solve(
