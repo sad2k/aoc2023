bbe22e7c4f46a5c5cc7526837586fb028dc5656e
Sergey Dubakov <serge.dubakov@gmail.com>
2024-01-31 20:50:46 +0000

    day 23 part 1

---
+++ src/bin/day23.rs
@@ -0,0 +1,91 @@
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fs,
+};
+
+#[derive(Debug)]
+struct Graph {
+    node_edges: HashMap<(i32, i32), Vec<(i32, i32)>>,
+}
+
+#[derive(Debug)]
+struct Path {
+    previous: HashSet<(i32, i32)>,
+    last: (i32, i32),
+}
+
+fn parse_graph(lines: &Vec<Vec<char>>) -> Graph {
+    let mut node_edges: HashMap<(i32, i32), Vec<(i32, i32)>> = HashMap::new();
+    for row in 0..lines.len() {
+        let line = &lines[row];
+        for col in 0..line.len() {
+            let edges = match line[col] {
+                '.' => {
+                    let mut v = Vec::new();
+                    if row > 0 && lines[row - 1][col] != '#' {
+                        v.push(((row - 1) as i32, col as i32));
+                    }
+                    if row < (lines.len() - 1) && lines[row + 1][col] != '#' {
+                        v.push(((row + 1) as i32, col as i32));
+                    }
+                    if col > 0 && lines[row][col - 1] != '#' {
+                        v.push((row as i32, (col - 1) as i32));
+                    }
+                    if col < (line.len() - 1) && lines[row][col + 1] != '#' {
+                        v.push((row as i32, (col + 1) as i32));
+                    }
+                    Some(v)
+                }
+                '>' => Some(vec![(row as i32, (col + 1) as i32)]),
+                '<' => Some(vec![(row as i32, (col - 1) as i32)]),
+                'v' => Some(vec![((row + 1) as i32, col as i32)]),
+                '^' => Some(vec![((row - 1) as i32, col as i32)]),
+                _ => None,
+            };
+            if edges.is_some() {
+                node_edges.insert((row as i32, col as i32), edges.unwrap());
+            }
+        }
+    }
+    Graph { node_edges }
+}
+
+fn part1(graph: &Graph, dest: (i32, i32)) -> i32 {
+    let mut q = VecDeque::new();
+    q.push_front(Path {
+        previous: HashSet::new(),
+        last: (0, 1),
+    });
+    let mut res = 0;
+    while !q.is_empty() {
+        let p = q.pop_front().unwrap();
+        if p.last == dest {
+            println!("Got to dest with {:?}", p.previous.len());
+            res = res.max(p.previous.len());
+        } else {
+            let mut new_prev = p.previous.clone();
+            new_prev.insert(p.last.clone());
+            for e in &graph.node_edges[&p.last] {
+                if !p.previous.contains(e) {
+                    q.push_front(Path {
+                        previous: new_prev.clone(),
+                        last: e.clone()
+                    });
+                }
+            }
+        }
+    }
+    res as i32
+}
+
+fn main() {
+    let content = fs::read_to_string("inputs/day23.txt").unwrap();
+    let lines = content
+        .lines()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let graph = parse_graph(&lines);
+
+    // part 1
+    println!("{}", part1(&graph, ((lines.len()-1) as i32, (lines[0].len()-2) as i32)));
+}
